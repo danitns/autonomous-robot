12d5f34071dc13087dba761f56509f927ad2da8a
danitns <dani.tanase2002@gmail.com>
2025-03-16 14:44:33 +0200

    changes

--- src/lsm9ds1_ros2_driver/lsm9ds1_ros2_driver/imu_data_publisher.py
+++ src/lsm9ds1_ros2_driver/lsm9ds1_ros2_driver/imu_data_publisher.py
@@ -1,9 +1,7 @@
-#!/home/danitns/Documents/imu_publisher/env_imu/bin/python3
-
+#!/home/danitns/Documents/autonomous-robot/pi-env/bin/python3
+import pigpio
 import time
-import board
-import adafruit_lsm9ds1
-from digitalio import DigitalInOut, Direction
+import struct 
 
 # Import ROS2 libraries
 import rclpy
@@ -11,88 +9,103 @@ from rclpy.node import Node
 from sensor_msgs.msg import Imu, MagneticField, Temperature
 from geometry_msgs.msg import Vector3
 
-magnometer_correction_x = 0.30030000000000007
-magnometer_correction_y = -0.27327999999999997
-magnometer_correction_z = 0.07140000000000002
+I2C_ADDR = 0x14  # STM32 I2C address
+BUS = 1  
 
-accel_correction_x = 0.14
-accel_correction_y = -0.4
-accel_correction_z = -0.11
+pi = pigpio.pi()
+handle = pi.i2c_open(BUS, I2C_ADDR)
 
-gyro_correction_x = 0.0238237442897226
-gyro_correction_y = 0.05062545661566052
-gyro_correction_z = -0.106581392688091176
-
-spi = board.SPI()
-csag = DigitalInOut(board.D5)
-csag.direction = Direction.OUTPUT
-csag.value = True
-csm = DigitalInOut(board.D6)
-csm.direction = Direction.OUTPUT
-csm.value = True
-sensor = adafruit_lsm9ds1.LSM9DS1_SPI(spi, csag, csm)
 
 class LSM9DS1Node(Node):
     def __init__(self):
         super().__init__("lsm9ds1_node")
-        self.imu_publisher = self.create_publisher(Imu, "imu/data_raw", 10)
-        self.magnetometer_publisher = self.create_publisher(MagneticField, "imu/mag", 10)
-        # self.temp_publisher = self.create_publisher(Temperature, "imu/temp", 10)
+        self.imu_publisher = self.create_publisher(Imu, "imu/data", 10)
+
+        # quaternion
+        self.q0 = 0
+        self.q1 = 0
+        self.q2 = 0
+        self.q3 = 0
+
+        # acceleration
+        self.ax = 0
+        self.ay = 0
+        self.az = 0
+
+        # gyro
+        self.gx = 0
+        self.gy = 0
+        self.gz = 0
 
         self.timer = self.create_timer(0.1, self.publish_sensor_data)
 
     def publish_sensor_data(self):
         # read data
-        accel_x, accel_y, accel_z = sensor.acceleration
-        mag_x, mag_y, mag_z = sensor.magnetic
-        gyro_x, gyro_y, gyro_z = sensor.gyro
+        (count, data) = pi.i2c_read_device(handle, 20) 
+
+        # Unpack the data into corresponding IMU values
+        if count > 0:
+            self.reconstruct_imu_data(data)
         now = self.get_clock().now()
-        # temp = sensor.temperature
-
-        # apply corrections
-        accel_x -= accel_correction_x
-        accel_y -= accel_correction_y
-        accel_z -= accel_correction_z
-
-        mag_x -= magnometer_correction_x
-        mag_y -= magnometer_correction_y
-        mag_z -= magnometer_correction_z
-
-        gyro_x -= gyro_correction_x
-        gyro_y -= gyro_correction_y
-        gyro_z -= gyro_correction_z
 
         # create IMU message
         imu_msg = Imu()
         imu_msg.header.stamp = now.to_msg()
         imu_msg.header.frame_id = "imu_link"
-        imu_msg.linear_acceleration.x = accel_x
-        imu_msg.linear_acceleration.y = accel_y * (-1)
-        imu_msg.linear_acceleration.z = accel_z
+        
+        # Set the linear acceleration (ax, ay, az)
+        imu_msg.linear_acceleration.x = self.ax
+        imu_msg.linear_acceleration.y = self.ay
+        imu_msg.linear_acceleration.z = self.az
 
-        imu_msg.angular_velocity.x = gyro_x
-        imu_msg.angular_velocity.y = gyro_y * (-1)
-        imu_msg.angular_velocity.z = gyro_z
+        # Set the angular velocity (gx, gy, gz)
+        imu_msg.angular_velocity.x = self.gx
+        imu_msg.angular_velocity.y = self.gy
+        imu_msg.angular_velocity.z = self.gz
 
-        # create magnetometer message
-        mag_msg = MagneticField()
-        mag_msg.header.stamp = now.to_msg()
-        mag_msg.header.frame_id = "imu_link"
-        mag_msg.magnetic_field.x = mag_x * (-1)
-        mag_msg.magnetic_field.y = mag_y * (-1)
-        mag_msg.magnetic_field.z = mag_z
+        # Set the orientation (quaternion)
+        imu_msg.orientation.w = self.q0
+        imu_msg.orientation.x = self.q1
+        imu_msg.orientation.y = self.q2
+        imu_msg.orientation.z = self.q3
 
-        # create temperature message
-        # temp_msg = Temperature()
-        # temp_msg.temperature = temp
+        imu_msg.orientation_covariance[0] = 0.01 
+        imu_msg.orientation_covariance[4] = 0.01
+        imu_msg.orientation_covariance[8] = 0.01
+
+        imu_msg.angular_velocity_covariance[0] = 0.01
+        imu_msg.angular_velocity_covariance[4] = 0.01
+        imu_msg.angular_velocity_covariance[8] = 0.01
+
+        imu_msg.linear_acceleration_covariance[0] = 0.01
+        imu_msg.linear_acceleration_covariance[4] = 0.01
+        imu_msg.linear_acceleration_covariance[8] = 0.01
+
 
         # publish messages
         self.imu_publisher.publish(imu_msg)
-        self.magnetometer_publisher.publish(mag_msg)
-        # self.temp_publisher.publish(temp_msg)
+
+    def reconstruct_imu_data(self, data):
+        # quaternion
+        self.q0 = struct.unpack_from('h', data, 0)[0] / 10000.0
+        self.q1 = struct.unpack_from('h', data, 2)[0] / 10000.0
+        self.q2 = struct.unpack_from('h', data, 4)[0] / 10000.0
+        self.q3 = struct.unpack_from('h', data, 6)[0] / 10000.0
+
+        # acceleration
+        self.ax = struct.unpack_from('h', data, 8)[0] / 1000.0
+        self.ay = struct.unpack_from('h', data, 10)[0] / 1000.0
+        self.az = struct.unpack_from('h', data, 12)[0] / 1000.0
+
+        # gyro
+        self.gx = struct.unpack_from('h', data, 14)[0] / 100.0
+        self.gy = struct.unpack_from('h', data, 16)[0] / 100.0
+        self.gz = struct.unpack_from('h', data, 18)[0] / 100.0
+
 
 
 def main(args=None):
+    
     rclpy.init(args=args)
     node = LSM9DS1Node()
     try:
--- src/robot-nav/launch/imu.launch.py
+++ src/robot-nav/launch/imu.launch.py
@@ -7,21 +7,12 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    imu_filter_params = os.path.join(get_package_share_directory('robot-nav'),'config','imu_filter.yaml')
     robot_localization_params = os.path.join(get_package_share_directory('robot-nav'),'config','ekf.yaml')
 
     imu_node_raw = Node(
             package='lsm9ds1_ros2_driver',
             executable='imu_data_publisher',
             parameters=[],
-         )
-
-    imu_filter_node = Node(
-            package='imu_filter_madgwick',
-            executable='imu_filter_madgwick_node',
-            name='imu_filter',
-            output='screen',
-            parameters=[imu_filter_params],
          )
 
     robot_localization_node = Node(
@@ -34,6 +25,5 @@ def generate_launch_description():
 
     return LaunchDescription([
         imu_node_raw,
-        imu_filter_node,
         robot_localization_node
     ])
